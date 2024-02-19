c83712b934d765c3e8fcc570c390701e7014f642
NHEL360 <nhelmeynard@gmail.com>
2024-02-19 16:19:22 +0800

    appsss

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("data.json")
       .then(response => response.json())
       .then(data => {
+        const ProductCardsContainer = document.getElementById("product-cards");
+        const ProductsItemsContainer = document.getElementById("cart-items");
+        const Products = {}; // Object to store cart items and their quantities
