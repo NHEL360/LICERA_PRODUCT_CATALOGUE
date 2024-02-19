e92c84a8a1ab88d58bc987022adab3285742ce70
NHEL360 <nhelmeynard@gmail.com>
2024-02-19 16:23:58 +0800

    jss

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ data.products.forEach(Product => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
+
+    <img src="${Product.Product_Image}" class="card-img-top" alt="${Product.Product_Name}">
+    <div class="card-body">
+      <h5 class="card-title">${Product.Product_Name}</h5>
+      <p class="card-text">${Product.Product_Description}</p>
+      <p class="card-text">Price: $${Product.Product_Price.toFixed(2)}</p>
+      <p class="card-text"><small class="text-body-secondary">Date Added: ${Product.Product_DateAdded}</small></p>
+      <p class="card-text"><small class="text-body-secondary">Expiration Date: ${Product.Product_ExpirationDate}</small></p>
+      <button class="btn btn-primary add-to-cart" data-product="${Product.Product_Name}">Add to Cart</button>
+    </div>
+  `;
+    ProductCardsContainer.appendChild(card);
+  });
